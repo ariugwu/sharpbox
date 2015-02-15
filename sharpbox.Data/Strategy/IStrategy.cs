840ff985031bad3871b0c5407c03f2f873c58120
ari@drybydesign.com <ari@drybydesign.com>
2015-02-15 13:50:39 -0500

    Continuing work on the Data project and integration

--- sharpbox.Data/Strategy/IStrategy.cs
+++ sharpbox.Data/Strategy/IStrategy.cs
@@ -4,10 +4,16 @@ namespace sharpbox.Data.Strategy
 {
     public interface IStrategy<T>
     {
+        Dictionary<string, object> AuxInfo { get; set; } 
+        IEnumerable<T> Entities { get; set; }
+
+        void Init();
+        
         IEnumerable<T> All();
         T Create(T entity);
         T Get(int id);
         T Update(T entity);
         void Delete(T entity);
+
     }
 }
