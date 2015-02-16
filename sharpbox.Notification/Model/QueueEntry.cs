a87a4b742f207866095dc0353141033034e24df1
ari@drybydesign.com <ari@drybydesign.com>
2015-02-16 15:42:34 -0500

    Forcing a sync.

---
+++ sharpbox.Notification/Model/QueueEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using sharpbox.Dispatch.Model;
+
+namespace sharpbox.Notification.Model
+{
+    public class QueueEntry
+    {
+        public int QueueEntryId { get; set; }
+        public PublisherNames PublisherName { get; set; }
+        public string Message { get; set; }
+        public bool WasSent { get; set; }
+        public DateTime SentDate { get; set; }
+        public int AttempNumber { get; set; }
+        public DateTime NextAttempTime { get; set; }
+        public string UserId { get; set; }
+    }
+}
