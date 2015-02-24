a643a064391fb390a81deaee44e8630303b17f23
ariugwu <ari@drybydesign.com>
2015-02-23 20:41:55 -0500

    Included untracked changes.

---
+++ sharpbox.Io.Strategy.Binary/BinaryStrategy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace sharpbox.Io.Strategy.Binary
+{
+    public class BinaryStrategy : IStrategy
+    {
+        public void Write<T>(string filePath, T objectToWrite, bool append = false) where T : new()
+        {
+            using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, objectToWrite);
+            }
+        }
+
+        public T Read<T>(string filePath) where T : new()
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
+        }
+
+        public void Delete<T>(string filePath) where T : new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
